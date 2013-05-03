928f75f0cfe74ee1c0aed95676e1bb46938c5b0a
Kevin King <kcking@mit.edu>
2013-05-03 00:00:15 -0400

    fixed issue 11

--- src/music/Concat.java
+++ src/music/Concat.java
@@ -30,6 +30,13 @@ public class Concat implements Music {
     }
     
     /**
+     * @return second piece in this concatenation
+     */
+    public Music second() {
+        return second;
+    }
+
+    /**
      * @return duration of this concatenation
      */
     public double duration() {
